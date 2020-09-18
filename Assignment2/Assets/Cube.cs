e21034c56e145ccb75d4e93425a484a769f2dbe8
highTrolla <nategibbsremenda@gmail.com>
2020-09-18 04:02:15 -0400

    a buncha changes. Need to work on Solitary

---
+++ Assignment2/Assets/Cube.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Cube : MonoBehaviour
+{
+    public bool isFlipped;
+    public Renderer Renderer;
+    public Manager manager;
+    public Vector2 gridPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFlipped = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Click()
+    {
+        Flip();
+    }
+
+    public void Flip()
+    {
+        transform.DOPunchScale(Vector3.one * 1.25f, 1f);
+
+        if (isFlipped)
+        {
+            Renderer.material.DOColor(Color.cyan, 1f);
+            isFlipped = false;
+        }
+        else
+        {
+            Renderer.material.DOColor(Color.red, 1f);
+            isFlipped = true;
+        }
+    }
+
+
+}
---
+++ Assignment2/Assets/Manager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Manager : MonoBehaviour
+{
+    public Cube cube1;
+    public Cube cube2;
+    public Cube cube3;
+    public Cube cube4;
+    public Cube cube5;
+    public Cube cube6;
+    public Cube cube7;
+    public Cube cube8;
+    public Cube cube9;
+    
+
+    public GameObject winner;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (cube1.isFlipped && cube2.isFlipped && cube3.isFlipped && cube4.isFlipped && cube5.isFlipped && cube6.isFlipped && cube7.isFlipped && cube8.isFlipped && cube9.isFlipped)
+        {
+            Win();
+        }
+
+        if (Input.GetKeyDown("[1]"))
+        {
+            cube1.Flip();
+            cube2.Flip();
+            cube4.Flip();
+        }
+        if (Input.GetKeyDown("[2]"))
+        {
+            cube1.Flip();
+            cube2.Flip();
+            cube3.Flip();
+            cube5.Flip();
+        }
+        if (Input.GetKeyDown("[3]"))
+        {
+            cube2.Flip();
+            cube3.Flip();
+            cube6.Flip();
+        }
+        if (Input.GetKeyDown("[4]"))
+        {
+            cube1.Flip();
+            cube4.Flip();
+            cube5.Flip();
+            cube7.Flip();
+        }
+        if (Input.GetKeyDown("[5]"))
+        {
+            cube2.Flip();
+            cube4.Flip();
+            cube5.Flip();
+            cube6.Flip();
+            cube8.Flip();
+        }
+        if (Input.GetKeyDown("[6]"))
+        {
+            cube3.Flip();
+            cube5.Flip();
+            cube6.Flip();
+            cube9.Flip();
+        }
+        if (Input.GetKeyDown("[7]"))
+        {
+            cube4.Flip();
+            cube7.Flip();
+            cube8.Flip();
+        }
+        if (Input.GetKeyDown("[8]"))
+        {
+            cube5.Flip();
+            cube7.Flip();
+            cube8.Flip();
+            cube9.Flip();
+        }
+        if (Input.GetKeyDown("[9]"))
+        {
+            cube6.Flip();
+            cube8.Flip();
+            cube9.Flip();
+        }
+    }
+
+    public void Win()
+    {
+        Instantiate(winner, transform.position, Quaternion.identity);
+        cube1.isFlipped = false;
+    }
+
+
+}
