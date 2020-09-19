134eb6bcc3cbc30fea2ec690503102555b7cfaf7
highTrolla <nategibbsremenda@gmail.com>
2020-09-19 19:12:59 -0400

    final commit

--- Assignment2/Assets/Manager.cs
+++ Assignment2/Assets/Manager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class Manager : MonoBehaviour
@@ -14,9 +15,11 @@ public class Manager : MonoBehaviour
     public Cube cube7;
     public Cube cube8;
     public Cube cube9;
-    
+
+    public static int winNumber;
 
     public GameObject winner;
+
 
     void Start()
     {
@@ -25,71 +28,58 @@ public class Manager : MonoBehaviour
 
     void Update()
     {
-        if (cube1.isFlipped && cube2.isFlipped && cube3.isFlipped && cube4.isFlipped && cube5.isFlipped && cube6.isFlipped && cube7.isFlipped && cube8.isFlipped && cube9.isFlipped)
+        if (winNumber == 9)
         {
             Win();
+            winNumber = 0;
         }
 
-        if (Input.GetKeyDown("[1]"))
-        {
-            cube1.Flip();
-            cube2.Flip();
-            cube4.Flip();
+        if (cube1.clickBool)
+        {            
+            Button1();
+            cube1.clickBool = false;
         }
-        if (Input.GetKeyDown("[2]"))
+        if (cube2.clickBool)
         {
-            cube1.Flip();
-            cube2.Flip();
-            cube3.Flip();
-            cube5.Flip();
+            Button2();
+            cube2.clickBool = false;
         }
-        if (Input.GetKeyDown("[3]"))
+        if (cube3.clickBool)
         {
-            cube2.Flip();
-            cube3.Flip();
-            cube6.Flip();
+            Button3();
+            cube3.clickBool = false;
         }
-        if (Input.GetKeyDown("[4]"))
+        if (cube4.clickBool)
         {
-            cube1.Flip();
-            cube4.Flip();
-            cube5.Flip();
-            cube7.Flip();
+            Button4();
+            cube4.clickBool = false;
         }
-        if (Input.GetKeyDown("[5]"))
+        if (cube5.clickBool)
         {
-            cube2.Flip();
-            cube4.Flip();
-            cube5.Flip();
-            cube6.Flip();
-            cube8.Flip();
+            Button5();
+            cube5.clickBool = false;
         }
-        if (Input.GetKeyDown("[6]"))
+        if (cube6.clickBool)
         {
-            cube3.Flip();
-            cube5.Flip();
-            cube6.Flip();
-            cube9.Flip();
+            Button6();
+            cube6.clickBool = false;
         }
-        if (Input.GetKeyDown("[7]"))
+        if (cube7.clickBool)
         {
-            cube4.Flip();
-            cube7.Flip();
-            cube8.Flip();
+            Button7();
+            cube7.clickBool = false;
         }
-        if (Input.GetKeyDown("[8]"))
+        if (cube8.clickBool)
         {
-            cube5.Flip();
-            cube7.Flip();
-            cube8.Flip();
-            cube9.Flip();
+            Button8();
+            cube8.clickBool = false;
         }
-        if (Input.GetKeyDown("[9]"))
+        if (cube9.clickBool)
         {
-            cube6.Flip();
-            cube8.Flip();
-            cube9.Flip();
+            Button9();
+            cube9.clickBool = false;
         }
+
     }
 
     public void Win()
@@ -98,5 +88,64 @@ public class Manager : MonoBehaviour
         cube1.isFlipped = false;
     }
 
-
+    public void Button1()
+    {
+        cube1.Flip();
+        cube2.Flip();
+        cube4.Flip();
+    }
+    public void Button2()
+    {
+        cube1.Flip();
+        cube2.Flip();
+        cube3.Flip();
+        cube5.Flip();
+    }
+    public void Button3()
+    {
+        cube2.Flip();
+        cube3.Flip();
+        cube6.Flip();
+    }
+    public void Button4()
+    {
+        cube1.Flip();
+        cube4.Flip();
+        cube5.Flip();
+        cube7.Flip();
+    }
+    public void Button5()
+    {
+        cube2.Flip();
+        cube4.Flip();
+        cube5.Flip();
+        cube6.Flip();
+        cube8.Flip();
+    }
+    public void Button6()
+    {
+        cube3.Flip();
+        cube5.Flip();
+        cube6.Flip();
+        cube9.Flip();
+    }
+    public void Button7()
+    {
+        cube4.Flip();
+        cube7.Flip();
+        cube8.Flip();
+    }
+    public void Button8()
+    {
+        cube5.Flip();
+        cube7.Flip();
+        cube8.Flip();
+        cube9.Flip();
+    }
+    public void Button9()
+    {
+        cube6.Flip();
+        cube8.Flip();
+        cube9.Flip();
+    }
 }
